4739e37ff418a4d64a775bb5af9b50532531c350
ayshaiqbal57 <iqbalaysha57@gmail.com>
2024-09-06 08:38:04 +0500

    Assignment 3

---
+++ Assisngment 3/main.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Question no# 1
+  List studentNames = [
+    "Ayesha",
+    "Yusra",
+    "Sakina",
+    "Munazza",
+    "Afifa",
+    "Nadia"
+  ];
+  print(studentNames);
+
+  // Question no# 2
+  List<String> daysName = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  print(daysName[6]);
+
+  //  Question no#3
+  List data = ["Ayesha", "Undergraduate", 151780, "A", 75.7];
+  print(data);
+}
